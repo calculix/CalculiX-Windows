cba09538557e2878dc989c1c3ed436c424f17381
Cesare Guardino <guardino@users.noreply.github.com>
2022-10-11 17:16:04 +0100

    Update CalculiXWindowsShell.bat to 2.20 plus minor visual corrections

--- src/etc/CalculiXWindowsShell.bat
+++ src/etc/CalculiXWindowsShell.bat
@@ -3,7 +3,7 @@ rem ############################################################################
 rem Name:          CalculiXWindowsShell.bat
 rem Description:   Script to start a Windows CMD prompt shell with CalculiX environment
 rem Author:        Cesare Guardino
-rem Last modified: 22 April 2016
+rem Last modified: 11 October 2022
 rem 
 rem GE CONFIDENTIAL INFORMATION © 2016 General Electric Company - All Rights Reserved
 rem #####################################################################################
@@ -12,10 +12,10 @@ set CALCULIX_ETC_DIR=%~dp0
 if #%CALCULIX_ETC_DIR:~-1%# == #\#  set CALCULIX_ETC_DIR=%CALCULIX_ETC_DIR:~0,-1%
 call "%CALCULIX_ETC_DIR%\CALCULIXWindowsEnvironment.bat"
 set CALCULIX_ETC_DIR=
-mode 160,40
-color 81
-echo ------------------------------
-echo Command shell for CalculiX 2.10
-echo ------------------------------
+rem mode 160,40
+rem color 30
+echo -------------------------------
+echo Command shell for CalculiX 2.20
+echo -------------------------------
 echo/
 cmd.exe
